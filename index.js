d64938b0898f8684d4caf183122667176744e1a6
miltonmb <milton.pasos@gmail.com>
2021-05-18 17:13:19 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function next(x) {
+    window.location.href = "http://127.0.0.1:5500/" + x + ".html";
+}
+
+function answer(resp) {
+    if (resp === "si") {
+        window.location.href = "https://www.youtube.com/watch?v=39o-RH2OlCU";
+    } else {
+        window.location.href = "https://miltonmb.github.io/valeriaproject/" + "6" + ".html";
+    }
+}
